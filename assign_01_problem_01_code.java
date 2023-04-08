f341748895af4a824a902863f90e02f415e68975
mahinourashraf <128333701+mahinourashraf@users.noreply.github.com>
2023-04-08 21:18:06 +0200

    Add files via upload

---
+++ assign_01_problem_01_code.java
@@ -0,0 +1,14 @@
+//Mahinour Ashraf Mohamed
+//20216495
+import java.util.Scanner;
+public class problem01 {
+
+    
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+ 
+		String name = input.next();
+           System.out.println("Hello, "+name) ;    
+    }
+    
+}
